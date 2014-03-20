0916d4820e9f2d17550f2f60c9e24639892c9931
Ben Blackburn <benjamin.blackburn@gmail.com>
2014-03-20 10:03:53 -0500

    returns pool names

--- thprd.js
+++ thprd.js
@@ -1,6 +1,8 @@
 var request = require('request');
 var cheerio = require('cheerio');
 
+days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
 pools = {
     'Aloha': 3,
     'Beaverton': 15,
@@ -11,15 +13,16 @@ pools = {
     'Sunset': 5,
     'Tualatin Hills': 2
 };
-days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
 for (pool in pools) {
-    var url = 'http://www.thprd.org/schedules/schedule.cfm?cs_id=' + pools[pool]	;
-    request(url, function(err, resp, body) {
-        if (err)
-            throw err;
-        $ = cheerio.load(body);
-        console.log(pool);
-        // TODO: scraping goes here!
-    });
+    var url = 'http://www.thprd.org/schedules/schedule.cfm?cs_id=' + pools[pool];
+    request(url, ( function(pool) {
+        return function(err, resp, body) {
+            if (err)
+                throw err;
+            $ = cheerio.load(body);
+            console.log(pool);
+            // TODO: scraping goes here!
+        }
+    } )(pool));
 }
