0dcb4d3a17a5818eec5e356d4ba289791292e2d3
Ben Blackburn <benjamin.blackburn@gmail.com>
2014-03-20 10:13:24 -0500

    returns csv file

--- thprd.js
+++ thprd.js
@@ -2,7 +2,6 @@ var request = require('request');
 var cheerio = require('cheerio');
 
 days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-
 pools = {
     'Aloha': 3,
     'Beaverton': 15,
@@ -13,16 +12,17 @@ pools = {
     'Sunset': 5,
     'Tualatin Hills': 2
 };
-
 for (pool in pools) {
     var url = 'http://www.thprd.org/schedules/schedule.cfm?cs_id=' + pools[pool];
-    request(url, ( function(pool) {
-        return function(err, resp, body) {
-            if (err)
-                throw err;
-            $ = cheerio.load(body);
-            console.log(pool);
-            // TODO: scraping goes here!
-        }
-    } )(pool));
+
+    request(url, (function(pool) { return function(err, resp, body) {
+        $ = cheerio.load(body);
+        $('#calendar .days td').each(function(day) {
+            $(this).find('div').each(function() {
+                event = $(this).text().trim().replace(/\s\s+/g, ',').split(',');
+                if (event.length >= 2 && (event[1].match(/open swim/i) || event[1].match(/family swim/i)))
+                    console.log(pool + ',' + days[day] + ',' + event[0] + ',' + event[1]);
+            });
+        });
+    }})(pool));
 }
